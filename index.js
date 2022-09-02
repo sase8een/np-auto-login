70dfe4ad71963308769beaa47d8121be3ae587d3
Sase <86785221+sase8een@users.noreply.github.com>
2022-09-03 00:46:47 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+    const browser = await puppeteer.launch({
+        headless: false
+    });
+    const page = await browser.newPage();
+    await page.setViewport({ width: 1366, height: 768});
+
+    await page.goto('https://www.protrack365.com/', { waitUntil: 'networkidle2' });
+
+    await page.screenshot({ path: 'prelogin.png' });
+
+    await page.type('#username_login', 'velammal');
+    await page.type('#password_login', 'demo1234');
+    await page.click('#btn-submit');
+
+    await page.screenshot({ path: 'postlogin.png' });
+
+    // await browser.close();
+})();
