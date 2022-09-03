4fdc3122ed769d5b3252c60d243d8fe8ab08058e
Sase <sase8een@gmail.com>
2022-09-03 10:03:51 +0530

    puppeteer build pack added

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const puppeteer = require('puppeteer');
 
 (async () => {
     const browser = await puppeteer.launch({
-        headless: false
+        headless: true,
+        args: ['--no-sandbox','--disable-setuid-sandbox']
     });
     const page = await browser.newPage();
     await page.setViewport({ width: 1366, height: 768});
