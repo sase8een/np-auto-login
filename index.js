76f457bb7e1bcf306258193bdd495f0de0b699eb
Sase <sase8een@gmail.com>
2022-09-03 11:29:11 +0530

    server js added

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
 const puppeteer = require('puppeteer');
 
-(async () => {
+const bot = (async () => {
     const browser = await puppeteer.launch({
         headless: true,
-        args: ['--no-sandbox','--disable-setuid-sandbox']
+        args: ['--no-sandbox', '--disable-setuid-sandbox']
     });
     const page = await browser.newPage();
-    await page.setViewport({ width: 1366, height: 768});
+    await page.setViewport({ width: 1366, height: 768 });
 
     await page.goto('https://www.protrack365.com/', { waitUntil: 'networkidle2' });
 
-    await page.screenshot({ path: 'prelogin.png' });
+    // await page.screenshot({ path: 'prelogin.png' });
 
     await page.type('#username_login', 'velammal');
     await page.type('#password_login', 'demo1234');
     await page.click('#btn-submit');
 
-    await page.screenshot({ path: 'postlogin.png' });
+    // await page.screenshot({ path: 'postlogin.png' });
 
-    // await browser.close();
+    await browser.close();
 })();
+
+module.exports = bot;
